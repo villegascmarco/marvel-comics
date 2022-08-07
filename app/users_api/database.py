f1534638cd441c2c7ee62671c20bfa6224b491db
Villegas Marco <villegascmarco@gmail.com>
2022-08-07 15:28:28 -0500

    add users database class

---
+++ app/users_api/database.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+class Database:
+    def __init__(self):
+        self.client = MongoClient('mongodb://root:root@mongo_container:27017')
+
+        database = 'marvel-comics'
+        collection = 'users'
+        cursor = self.client[database]
+        self.collection = cursor[collection]
+
+    def read(self, credentials):
+        return self.collection.find_one(credentials)
+
+    def write(self, new_document):
+        response = self.collection.insert_one(new_document)
+        new_document['id'] = str(response.inserted_id)
+        return dumps(new_document)
