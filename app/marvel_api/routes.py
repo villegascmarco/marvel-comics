f87e9e8d1842e82ed8b2bc7a5565d9f6e9667bc8
Villegas Marco <villegascmarco@gmail.com>
2022-08-07 14:40:30 -0500

    clean marvel_api/routes file
    - Unnecessary comments were deleted

--- app/marvel_api/routes.py
+++ app/marvel_api/routes.py
@@ -13,39 +13,3 @@ def index():
         output['error'] = True
         output['message'] = str(error)
     return jsonify(output)
-
-
-# from textwrap import indent
-# from connector import Connector
-# from flask import Blueprint, jsonify, request
-# from bson.json_util import dumps
-
-# marvel_route = Blueprint('marvel_route', __name__, url_prefix='/marvel')
-
-
-# @marvel_route.route('/searchComics', methods=['POST'])
-# def index():
-#     user_ip = request.remote_addr
-
-#     return jsonify({
-#         "a": user_ip
-#     })
-
-
-# @marvel_route.route('list', methods=['GET'])
-# def list():
-#     obj = Connector(request.json)
-#     return jsonify({
-#         'response': dumps(obj.read(), indent=2)
-#     })
-
-
-# @marvel_route.route('create', methods=['POST'])
-# def create():
-#     print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-#     print(request.json)
-#     print('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
-#     obj = Connector(request.json)
-#     return jsonify({
-#         'response': obj.write(request.json)
-#     })
