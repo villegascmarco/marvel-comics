d22863dd944faa99b60065584ee37272ec535b7d
Villegas Marco <villegascmarco@gmail.com>
2022-08-06 23:56:39 -0500

    update project structure

--- app/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from routes.marvel_route import marvel_route
-
-
-if __name__ == '__main__':
-    app = Flask(__name__)
-
-    app.register_blueprint(marvel_route)
-    app.run(debug=True, host='0.0.0.0')
