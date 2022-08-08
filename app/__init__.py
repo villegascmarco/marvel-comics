cea274ffdefab630df6527a2529dfe4d5f2b23a7
Villegas Marco <villegascmarco@gmail.com>
2022-08-08 17:51:58 -0500

    add layaway_list_api blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,10 @@ def create_app():
         from .marvel_api.routes import marvel_route
         from .users_api.routes import users_route
         from app.layaway_api.routes import layaway_route
+        from app.layaway_list_api.routes import layaway_list_route
 
         app.register_blueprint(marvel_route)
         app.register_blueprint(users_route)
         app.register_blueprint(layaway_route)
+        app.register_blueprint(layaway_list_route)
         return app
