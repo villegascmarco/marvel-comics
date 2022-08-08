2aeaecb8439d9b6ce7bdcf1eaf295429a79bb666
Villegas Marco <villegascmarco@gmail.com>
2022-08-08 12:06:03 -0500

    add layaway route

---
+++ app/layaway_api/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify, request
+from app.layaway_api import decorator as session
+from . import controller
+
+layaway_route = Blueprint('layaway_route', __name__,
+                          url_prefix='/addToLayaway')
+
+
+@layaway_route.route('/', methods=['POST'])
+@session.validate_access()
+def add(current_user):
+    output = {}
+    try:
+        output['action'], output['result'] = controller.add_to_layaway(
+            current_user, request)
+    except Exception as error:
+        output['error'] = True
+        output['message'] = str(error)
+    return str(output)
