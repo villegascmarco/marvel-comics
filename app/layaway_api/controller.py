446eebd54b590f5f2ad53856e77e8af35896d929
Villegas Marco <villegascmarco@gmail.com>
2022-08-08 16:07:01 -0500

    update layaway controller to match new db logic
    - Extra user data has added to layaway response

--- app/layaway_api/controller.py
+++ app/layaway_api/controller.py
@@ -1,7 +1,7 @@
 from app.marvel_api.controller import search_comic_id
+from app.users_api.controller import find_by_id
 from app.layaway_api.database import Database
 from app.utilities.json import get_or_error
-from bson.objectid import ObjectId
 from bson.json_util import dumps
 import time
 
@@ -20,17 +20,21 @@ def add_to_layaway(current_user, request):
             raise Exception(
                 f'Comic with id {comic_id} does not exist. Only existing data in Marvel page is permited. Nothing was updated.')
     db = Database()
-    # return "", dumps(data)
+
     if db.read({"user_id": current_user}):
         # Update
-        output = db.update({"user_id": current_user},
-                           {'$push': {'comics': {'$each': data}}})
-        return 'User info updated succesfully', dumps(output)
+        db.update({"user_id": current_user},
+                  {'$push': {'comics': {'$each': data}}})
+        action = 'User info updated succesfully.'
     else:
         # Create
-        output = db.write({"user_id": current_user, "comics": data})
-        return "User info saved succesfully.", dumps(output)
+        db.write({"user_id": current_user, "comics": data})
+        action = 'User info saved succesfully.'
 
+    output = db.read({"user_id": current_user})
+    user = find_by_id(output['user_id'])
+    user.pop('password', None)
+    output['user'] = user
+    output.pop('user_id', None)
 
-def get_layaways_user(curren_user, request):
-    pass
+    return action, dumps(output)
